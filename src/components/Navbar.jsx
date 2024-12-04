9bca825985ea4477a656558303595fa259b02227
Patryk Bochenek <patryk.bochenek28@gmail.com>
2024-12-04 18:33:30 +0100

    Added navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div>Logo</div>
+      <div>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
