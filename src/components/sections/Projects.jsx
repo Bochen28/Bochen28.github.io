50d56710e972f4e0e1e89e9d1df7c70c58fc96fd
Patryk Bochenek <patryk.bochenek28@gmail.com>
2024-12-19 18:47:26 +0100

    Added project wrappers

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,10 +1,48 @@
+import Image from "next/image";
 import styles from "@/styles/projects.module.sass";
 
 export default function Projects() {
   return (
     <div id="projects" className={styles.sectionWrapper}>
       <h1>Let's look at my work</h1>
-      <div><h2>Project example</h2></div>
+      <div className={styles.projectsWrapper}>
+        <div className={styles.project}>
+          <Image
+            className={styles.projectImage}
+            src="/project1.png"
+            alt="Project 1"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className={styles.project}>
+          <Image
+            className={styles.projectImage}
+            src="/project1.png"
+            alt="Project 1"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className={styles.project}>
+          <Image
+            className={styles.projectImage}
+            src="/project1.png"
+            alt="Project 1"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className={styles.project}>
+          <Image
+            className={styles.projectImage}
+            src="/project1.png"
+            alt="Project 1"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
     </div>
   );
 }
