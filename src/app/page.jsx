b62c2f5244d96d717414eaa527ca499e72dab406
Bochen28 <patryk.bochenek28@gmail.com>
2024-12-24 18:53:00 +0100

    Changed globals.css to sass

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,6 @@ import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
 import Projects from "@/components/sections/Projects";
 import Contact from "@/components/sections/Contact";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
